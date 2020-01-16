05b95102b9be1a16d6ab5791e0992182ad08a8e1
Budin Kolyandov <budin_kolyandov@abv.bg>
2020-01-16 20:05:02 +0200

    Automated the registration of the routes.

--- Basic/SIS/IRunes.App/Controllers/HomeController.cs
+++ Basic/SIS/IRunes.App/Controllers/HomeController.cs
@@ -2,11 +2,18 @@
 using SIS.HTTP.Requests;
 using SIS.HTTP.Responses;
 using SIS.MvcFramework;
+using SIS.MvcFramework.Attributes;
 
 namespace IRunes.App.Controllers
 {
     public class HomeController : Controller
     {
+        [HttpGet(Url = "/")]
+        public IHttpResponse IndexSlash(IHttpRequest request)
+        {
+            return Index(request);
+        }
+
         public IHttpResponse Index(IHttpRequest request)
         {
             if (this.IsLoggedIn(request))
---
+++ Basic/SIS/WebServer/Attributes/HttpDeleteAttribute.cs
@@ -0,0 +1,12 @@
+﻿using SIS.HTTP.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SIS.MvcFramework.Attributes
+{
+    public class HttpDeleteAttribute : BaseHttpAttribute
+    {
+        public override HttpRequestMethod Method => HttpRequestMethod.Delete;
+    }
+}
