d361322d49c434858e3079a64789c62b837da1fc
Budin Kolyandov <budin_kolyandov@abv.bg>
2020-01-29 10:52:49 +0200

    Created the inmemory compiler for the view engine.

--- Basic/SIS/WebServer/ViewEngine/SisViewEngine.cs
+++ Basic/SIS/WebServer/ViewEngine/SisViewEngine.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
 
 namespace SIS.MvcFramework.ViewEngine
 {
@@ -17,12 +22,13 @@ using SIS.MvcFramework.ViewEngine;
 
 namespace AppViewCodeNamespace
 {{
-    public class AppVIewCode : IView
+    public class AppViewCode : IView
     {{
         public string GetHtml()
         {{
             var html = new StringBuilder();
-            
+            html.Append(""Hello from memory!!!"");
+
             {csharpHtmlCode}
 
             return html.ToString();
@@ -38,12 +44,59 @@ namespace AppViewCodeNamespace
 
         private IView CompileAndInstance(string code)
         {
-            throw new NotImplementedException();
+            var compilation = CSharpCompilation.Create("AppViewAssembly")
+                .WithOptions(new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary))
+                .AddReferences(MetadataReference
+                .CreateFromFile(typeof(object).Assembly.Location))
+                .AddReferences(MetadataReference
+                .CreateFromFile(typeof(IView).Assembly.Location));
+
+            var netStandardAssembly = Assembly.Load(new AssemblyName("netstandard")).GetReferencedAssemblies();
+
+            foreach (var assembly in netStandardAssembly)
+            {
+                compilation = compilation.AddReferences(MetadataReference.CreateFromFile(Assembly.Load(assembly).Location));
+            }
+
+            compilation = compilation.AddSyntaxTrees(SyntaxFactory.ParseSyntaxTree(code));
+
+            using (var memoryStream = new MemoryStream())
+            {
+                var compilationResult = compilation.Emit(memoryStream);
+                if (!compilationResult.Success)
+                {
+                    foreach (var error in compilationResult.Diagnostics)
+                    {
+                        Console.WriteLine(error.GetMessage());
+                    }
+                    return null;
+                }
+                memoryStream.Seek(0, SeekOrigin.Begin);
+                var assemblyBytes = memoryStream.ToArray();
+                var assembly = Assembly.Load(assemblyBytes);
+
+                var type = assembly.GetType("AppViewCodeNamespace.AppViewCode");
+                if (type == null)
+                {
+                    Console.WriteLine("AppViewCore not found!");
+                    return null;
+                }
+
+                var instance = Activator.CreateInstance(type);
+                if (instance == null)
+                {
+                    Console.WriteLine("AppViewCore can't be instanciated!");
+                    return null;
+                }
+
+                return instance as IView;
+            }
+            
         }
 
         private string GetCsharpCode(string viewContent)
         {
-            throw new NotImplementedException();
+            return null;
         }
     }
 }
